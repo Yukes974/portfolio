f712b00d57c5b345f21d859108d867aa2e885a2f
Patrick CHARLETTE <charlettepatrick@gmail.com>
2024-04-26 16:42:31 +0400

    Maj des sections

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,23 @@
 import "../../css/home.css";
-import Thumbnail from "../../components/thumbnail";
+import Thumbnails from "../../components/thumbnail";
 import ProjectJson from "../../project.json";
 
 function Home() {
   return (
-    <div>
-      <div>
-        <img src="" alt="" />
-      </div>
-      <h1>Patrick CHARLETTE</h1>
-      <h2>Web Developper Junior</h2>
+    <section className="sectionpersonprojects">
       <section>
-        <h3>My Projects</h3>
-        <Thumbnail projectCard={ProjectJson} />
+        <div className="mypicture">
+          <img src="" alt="" />
+        </div>
+        <h1>Patrick CHARLETTE</h1>
+        <h2>Web Developper Junior</h2>
       </section>
-    </div>
+
+      <section className="sectionprojects">
+        <h3>My Projects</h3>
+        <Thumbnails projectCard={ProjectJson} />
+      </section>
+    </section>
   );
 }
 
